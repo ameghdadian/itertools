54b97b85021a3d075f27f342f7005b7b2c03b0fe
ameghdadian <amirthedevops@gmail.com>
2025-03-05 18:56:47 +0330

    Add helper tools to work with iterators

---
+++ itertools.go
@@ -0,0 +1,126 @@
+// Package itertools provides utility functions to work with iterators.
+package itertools
+
+import (
+	"cmp"
+	"iter"
+	"math/rand/v2"
+	"slices"
+)
+
+// Concat receives an arbitrary number of slices and returns an iterator to
+// iterate on them successively, in the order they are given.
+func Concat[Slice []T, T any](seqs ...Slice) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for _, seq := range seqs {
+			for _, v := range seq {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// ConcatIter receives an arbitrary number of slices and returns an iterator to
+// iterate over the elements, in the order they are given.
+func ConcatIter[T any](seqs ...iter.Seq[T]) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for _, seq := range seqs {
+			for v := range seq {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// Reverse receives an arbitrary number of slices and returns an iterator to
+// iterate over the elements, in the reverse order they are given.
+//
+// For example, given the input:
+//
+//	Reverse([]int{10, 20, 30}, []int{40, 50, 60})
+//
+// The iterator following the output:
+//
+//	{60, 50, 40, 30, 20, 10}
+func Reverse[Slice []T, T any](seqs ...Slice) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for i := len(seqs) - 1; i >= 0; i-- {
+			for j := len(seqs[i]) - 1; j >= 0; j-- {
+				if !yield(seqs[i][j]) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// Shuffle receives a slice and returns an iterator over its shuffled elements.
+//
+// Note: In order not to allocate extra memory, it shuffles given slice(array) in place.
+// If you need the original slice(array) untouched, you can clone your slice using [slices.Clone]
+// and then pass it as the argument.
+func Shuffle[Slice []T, T any](seq Slice) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		swapFunc := func(i int, j int) {
+			seq[i], seq[j] = seq[j], seq[i]
+		}
+		rand.Shuffle(len(seq), swapFunc)
+		for v := range slices.Values(seq) {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+// Filter returns an iterator that contains the elements of seq for which filterFn
+// returns true.
+func Filter[Slice []T, T comparable](seq Slice, filterFn func(T) bool) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for _, v := range seq {
+			if filterFn(v) {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// Map executes a user-supplied function on each element of the slice and returns
+// an iterator over modified elements.
+func Map[Slice []T, T any](seq Slice, mapFn func(int, T) T) iter.Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		for i, v := range seq {
+			if !yield(i, mapFn(i, v)) {
+				return
+			}
+		}
+	}
+}
+
+// ForEach executes a user-supplied function on each element of the sequence. It does not
+// return any output.
+func ForEach[T any](seq iter.Seq[T], fn func(int, T)) {
+	var indx int
+	for v := range seq {
+		fn(indx, v)
+		indx += 1
+	}
+}
+
+// Reduce executes a user-supplied "reducer" function on each element of the array,
+// in order, passing in the return value from the calculation on the preceding element.
+func Reduce[T cmp.Ordered](seq iter.Seq[T], reducer func(acc T, cur T) T, init T) T {
+	var total T
+
+	for v := range seq {
+		init += reducer(total, v)
+	}
+
+	return init
+}
---
+++ itertools_test.go
@@ -0,0 +1,371 @@
+package itertools
+
+import (
+	"cmp"
+	"fmt"
+	"io"
+	"iter"
+	"math"
+	"os"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func TestConcat(t *testing.T) {
+	type TestCase[T any] struct {
+		input    [][]T
+		expected []T
+	}
+
+	table := []TestCase[any]{
+		{
+			input:    [][]any{{1, 2, 3}, {7, 8, 9}},
+			expected: []any{1, 2, 3, 7, 8, 9},
+		},
+		{
+			input:    [][]any{{"hello", "world"}, {"gophers"}},
+			expected: []any{"hello", "world", "gophers"},
+		},
+		{
+			input:    [][]any{{1.2, 10.44}, {5.1}},
+			expected: []any{1.2, 10.44, 5.1},
+		},
+	}
+
+	for _, tt := range table {
+		var a []any
+		for v := range Concat(tt.input...) {
+			a = append(a, v)
+		}
+
+		if !slices.Equal(a, tt.expected) {
+			t.Errorf("not equal")
+			t.Logf("got: %v", a)
+			t.Logf("exp: %v", tt.expected)
+		}
+	}
+}
+
+func TestConcatIter(t *testing.T) {
+	type TestCase[T iter.Seq[any], V any] struct {
+		input    []T
+		expected []V
+	}
+
+	table := []TestCase[iter.Seq[any], any]{
+		{
+			input:    []iter.Seq[any]{slices.Values([]any{1, 2, 3}), slices.Values([]any{7, 8, 9})},
+			expected: []any{1, 2, 3, 7, 8, 9},
+		},
+		{
+			input:    []iter.Seq[any]{slices.Values([]any{"hello", "world"}), slices.Values([]any{"gophers"})},
+			expected: []any{"hello", "world", "gophers"},
+		},
+		{
+			input:    []iter.Seq[any]{slices.Values([]any{1.2, 10.44}), slices.Values([]any{5.1})},
+			expected: []any{1.2, 10.44, 5.1},
+		},
+	}
+
+	for _, tt := range table {
+		var a []any
+		for v := range ConcatIter(tt.input...) {
+			a = append(a, v)
+		}
+
+		if !slices.Equal(a, tt.expected) {
+			t.Errorf("not equal")
+			t.Logf("got: %v", a)
+			t.Logf("exp: %v", tt.expected)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+	type TestCase[T any] struct {
+		input    [][]T
+		expected []T
+	}
+
+	table := []TestCase[any]{
+		{
+			input:    [][]any{{1, 2, 3}, {7, 8, 9}},
+			expected: []any{9, 8, 7, 3, 2, 1},
+		},
+		{
+			input:    [][]any{{"hello", "world"}, {"gophers"}},
+			expected: []any{"gophers", "world", "hello"},
+		},
+		{
+			input:    [][]any{{1.2, 10.44}, {5.1}},
+			expected: []any{5.1, 10.44, 1.2},
+		},
+	}
+
+	for _, tt := range table {
+		var a []any
+		for v := range Reverse(tt.input...) {
+			a = append(a, v)
+		}
+
+		if !slices.Equal(a, tt.expected) {
+			t.Errorf("not equal")
+			t.Logf("got: %v", a)
+			t.Logf("exp: %v", tt.expected)
+		}
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	type TestCase[T any] struct {
+		input         [][]T
+		shouldContain []T
+	}
+
+	table := []TestCase[any]{
+		{
+			input:         [][]any{{1, 2, 3}, {7, 8, 9}},
+			shouldContain: []any{9, 8, 7, 3, 2, 1},
+		},
+		{
+			input:         [][]any{{"hello", "world"}, {"gophers"}},
+			shouldContain: []any{"gophers", "world", "hello"},
+		},
+		{
+			input:         [][]any{{1.2, 10.44}, {5.1}},
+			shouldContain: []any{5.1, 10.44, 1.2},
+		},
+	}
+
+	for _, tt := range table {
+		var a []any
+		for v := range Reverse(tt.input...) {
+			a = append(a, v)
+		}
+
+		if len(a) != len(tt.shouldContain) {
+			t.Errorf("length not equal")
+			t.Logf("got: %d", len(a))
+			t.Logf("exp: %d", len(tt.shouldContain))
+		}
+		for _, v := range tt.shouldContain {
+			if !slices.Contains(a, v) {
+				t.Errorf("element not found")
+				t.Logf("exp: %v", v)
+			}
+		}
+	}
+}
+
+func TestFilter(t *testing.T) {
+	type TestCase[T comparable] struct {
+		input    []T
+		expected []T
+		fn       func(T) bool
+	}
+
+	table := []TestCase[any]{
+		{
+			input:    []any{1, 2, 3, 7, 8, 9},
+			expected: []any{2, 8},
+			fn: func(v any) bool {
+				val, _ := v.(int)
+				return val%2 == 0
+			},
+		},
+		{
+			input:    []any{"hello", "world", "gophers"},
+			expected: []any{"hello", "world"},
+			fn: func(v any) bool {
+				val, _ := v.(string)
+				return val != "gophers"
+			},
+		},
+		{
+			input:    []any{1.2, 10.44, 5.1},
+			expected: []any{10.44, 5.1},
+			fn: func(v any) bool {
+				val, _ := v.(float64)
+				return val != 1.2
+			},
+		},
+	}
+
+	for _, tt := range table {
+		var a []any
+		for v := range Filter(tt.input, tt.fn) {
+			a = append(a, v)
+		}
+
+		if !slices.Equal(a, tt.expected) {
+			t.Errorf("not equal")
+			t.Logf("got: %v", a)
+			t.Logf("exp: %v", tt.expected)
+		}
+	}
+}
+
+func TestMap(t *testing.T) {
+	type TestCase[T comparable] struct {
+		input    []T
+		expected []T
+		fn       func(int, T) T
+	}
+
+	table := []TestCase[any]{
+		{
+			input:    []any{1, 2, 3, 7, 8, 9},
+			expected: []any{2, 4, 6, 14, 16, 18},
+			fn: func(_ int, v any) any {
+				val, _ := v.(int)
+				return val * 2
+			},
+		},
+		{
+			input:    []any{"hello", "world", "gophers"},
+			expected: []any{"hello💣", "world💣", "gophers💣"},
+			fn: func(_ int, v any) any {
+				val, _ := v.(string)
+				return val + "💣"
+			},
+		},
+		{
+			input:    []any{1.2, 10.44, 5.1},
+			expected: []any{2.4, 20.88, 10.2},
+			fn: func(_ int, v any) any {
+				val, _ := v.(float64)
+				return val * 2
+			},
+		},
+	}
+
+	for _, tt := range table {
+		var a []any
+		for _, v := range Map(tt.input, tt.fn) {
+			a = append(a, v)
+		}
+
+		if !slices.Equal(a, tt.expected) {
+			t.Errorf("not equal")
+			t.Logf("got: %v", a)
+			t.Logf("exp: %v", tt.expected)
+		}
+	}
+}
+
+func TestForEach(t *testing.T) {
+	type TestCase[T comparable] struct {
+		input []T
+		exp   string
+		fn    func(int, T)
+	}
+
+	table := []TestCase[any]{
+		{
+			input: []any{1, 2, 3, 7, 8, 9},
+			exp:   "246141618",
+			fn: func(_ int, v any) {
+				val, _ := v.(int)
+				fmt.Print(val * 2)
+			},
+		},
+		{
+			input: []any{"hello", "world", "gophers"},
+			exp:   "hello💣world💣gophers💣",
+			fn: func(_ int, v any) {
+				val, _ := v.(string)
+				fmt.Print(val + "💣")
+			},
+		},
+		{
+			input: []any{1.2, 10.44, 5.1},
+			exp:   "2.420.8810.2",
+			fn: func(_ int, v any) {
+				val, _ := v.(float64)
+				fmt.Print(val * 2)
+			},
+		},
+	}
+
+	for _, tt := range table {
+		prev := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		ForEach(slices.Values(tt.input), tt.fn)
+		w.Close()
+		os.Stdout = prev
+
+		msg, err := io.ReadAll(r)
+		if err != nil {
+			t.Error(err)
+		}
+		r.Close()
+
+		if !strings.EqualFold(string(msg), tt.exp) {
+			t.Error("not equal")
+			t.Logf("got: %v", string(msg))
+			t.Logf("exp: %v", tt.exp)
+		}
+
+	}
+}
+
+type ReduceTestCase[T cmp.Ordered] struct {
+	input    []T
+	expected T
+	fn       func(T, T) T
+	init     T
+}
+
+func reduceTestHelper[V cmp.Ordered](table ReduceTestCase[V]) func(t *testing.T) {
+	return func(t *testing.T) {
+		result := Reduce(slices.Values(table.input), table.fn, table.init)
+
+		f, ok := any(result).(float64)
+		if ok {
+			rounded := math.Round(f*10) / 10
+			result = any(rounded).(V)
+		}
+
+		if result != table.expected {
+			t.Errorf("not equal")
+			t.Logf("got: %v", result)
+			t.Logf("exp: %v", table.expected)
+		}
+	}
+}
+
+func TestReduce(t *testing.T) {
+	table := ReduceTestCase[int]{
+		input:    []int{1, 2, 3, 7, 8, 9},
+		expected: 30,
+		fn: func(acc int, cur int) int {
+			return acc + cur
+		},
+		init: 0,
+	}
+
+	table2 := ReduceTestCase[string]{
+		input:    []string{"hello", "world", "gophers"},
+		expected: ":)helloworldgophers",
+		fn: func(acc string, cur string) string {
+			return acc + cur
+		},
+		init: ":)",
+	}
+
+	table3 := ReduceTestCase[float64]{
+		input:    []float64{1.2, 10.4, 5.1},
+		expected: 18.7,
+		fn: func(acc float64, cur float64) float64 {
+			return acc + cur
+		},
+		init: 2.0,
+	}
+
+	t.Run("int", reduceTestHelper(table))
+	t.Run("string", reduceTestHelper(table2))
+	t.Run("float64", reduceTestHelper(table3))
+}
